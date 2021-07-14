812a9332a3766bca91853bcf19cf15f8fe9b2ba2
Minebot <serpinin@mail.ru>
2021-07-14 12:23:23 +0300

    postgre connect

--- src/main/java/ru/minebot/gameshop/GameShopApplication.java
+++ src/main/java/ru/minebot/gameshop/GameShopApplication.java
@@ -1,16 +1,19 @@
 package ru.minebot.gameshop;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.List;
+import java.util.Map;
 
 @SpringBootApplication
 public class GameShopApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GameShopApplication.class, args);
-
 	}
 
 }
--- src/main/java/ru/minebot/gameshop/controller/MainController.java
+++ src/main/java/ru/minebot/gameshop/controller/MainController.java
@@ -1,9 +1,14 @@
 package ru.minebot.gameshop.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+import java.util.Map;
 
 @Controller
 public class MainController {
